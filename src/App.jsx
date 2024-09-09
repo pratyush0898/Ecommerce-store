6c6b88eac3ec881e86e2edc5ac228ff7abefe75c
Pratyush0898 <priyankarangwa@gmail.com>
2024-09-09 15:26:09 +0530

    Create project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Page/Home/Home";
-import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Login from "./Page/Login/Login";
+import { auth } from './firebase/firebase.js'
+import Checkout from "./Page/Checkout/Checkout.jsx";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <div className="app">
       <Router>
@@ -13,6 +37,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
         </Routes>
       </Router>
     </div>
